a8cc1345a4ca24db001b959c50e1a833a9cb682f
sakshi7777 <sakshi107gupta@gmail.com>
2022-03-21 12:25:36 +0530

    added search page code

---
+++ src/Automation/searchtest.java
@@ -0,0 +1,12 @@
+package Automation;
+
+public class searchtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("search page");
+	}
+
+}
